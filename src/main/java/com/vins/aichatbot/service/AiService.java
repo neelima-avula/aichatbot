aebe3eb936f746306776be0fbf8d8d608e7392c4
Neelima Avula <neelima@MacBook-Pro-3.local>
2025-12-03 00:54:18 -0500

    updated ai service 5

--- src/main/java/com/vins/aichatbot/service/AiService.java
+++ src/main/java/com/vins/aichatbot/service/AiService.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import com.vins.aichatbot.model.AiResponse;
-
 @Service
 public class AiService {
 
